a96ba835f085c6fd43a414da1e6c42fa24f1d0ee
JoseRoj <jorojas2019@udec.cl>
2024-05-21 14:33:29 -0400

    :construction: Avance Sprint 2 0.1

--- frontend/clubconnect/ios/Runner/AppDelegate.swift
+++ frontend/clubconnect/ios/Runner/AppDelegate.swift
@@ -8,7 +8,6 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyBj_7pVU0nBUMWOUnc_4WPLCVuz4rgWmOM")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
